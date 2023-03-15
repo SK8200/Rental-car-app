81902a3992e86801db1d017f9676e5fb3a8ae836
Sagar Kalani <sagar@york.ie>
2023-03-15 18:13:20 +0530

    app Done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,88 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+// import NavBar from "./ui-components/NavBar";
+import { NewCarsCollection, NavBar, AddCar } from "./ui-components";
+import { withAuthenticator, Divider } from "@aws-amplify/ui-react";
+import { useState } from "react";
+import { DataStore } from "aws-amplify";
+import { RentalCar } from "./models";
 
-function App() {
+function App({ user, signOut }) {
+  const [addCar, setAddCar] = useState({
+    name: "",
+    price: "",
+    description: "",
+    imageUrl: "",
+  });
+
+  const saveRentalCar = async () => {
+    try {
+      await DataStore.save(new RentalCar(addCar));
+      setAddCar({
+        name: "",
+        price: "",
+        description: "",
+        imageUrl: "",
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const addCarOverrides = {
+    TextField29766922: {
+      value: addCar.name,
+      onChange: (e) => {
+        setAddCar({ ...addCar, name: e.target.value });
+      },
+    },
+
+    TextField29766923: {
+      value: addCar.price,
+      onChange: (e) => {
+        setAddCar({ ...addCar, price: e.target.value });
+      },
+    },
+    TextField29766924: {
+      value: addCar.description,
+      onChange: (e) => {
+        setAddCar({ ...addCar, description: e.target.value });
+      },
+    },
+    TextField36462711: {
+      value: addCar.imageUrl,
+      onChange: (e) => {
+        setAddCar({ ...addCar, imageUrl: e.target.value });
+      },
+    },
+    Button: {
+      onClick: saveRentalCar,
+    },
+  };
+
+  const navbarOverrides = {
+    "image 1": {
+      src: "https://media.istockphoto.com/id/165677538/vector/black-white-1969-chevelle-ss.jpg?s=1024x1024&w=is&k=20&c=zYzQlOq25Z2Zceepws2SfXnhoNLMEjJeaxUd2aNl6QM=",
+    },
+    image: {
+      src: user?.attributes?.profile,
+    },
+    Button: {
+      onClick: signOut,
+    },
+  };
   return (
     <div className="App">
+      <NavBar overrides={navbarOverrides} />
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <AddCar
+          style={{ textAlign: "left", margin: "1rem" }}
+          overrides={addCarOverrides}
+        />
+        <Divider />
+        <NewCarsCollection />
       </header>
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
---
+++ src/ui-components/AddCar.jsx
@@ -0,0 +1,172 @@
+/***************************************************************************
+ * The contents of this file were generated with Amplify Studio.           *
+ * Please refrain from making any modifications to this file.              *
+ * Any changes to this file will be overwritten when running amplify pull. *
+ **************************************************************************/
+
+/* eslint-disable */
+import * as React from "react";
+import { getOverrideProps } from "@aws-amplify/ui-react/internal";
+import { Button, Divider, Flex, Text, TextField } from "@aws-amplify/ui-react";
+export default function AddCar(props) {
+  const { overrides, ...rest } = props;
+  return (
+    <Flex
+      gap="16px"
+      direction="column"
+      width="640px"
+      height="unset"
+      justifyContent="flex-start"
+      alignItems="flex-start"
+      position="relative"
+      padding="0px 0px 0px 0px"
+      backgroundColor="rgba(255,255,255,1)"
+      {...getOverrideProps(overrides, "AddCar")}
+      {...rest}
+    >
+      <Flex
+        gap="24px"
+        direction="column"
+        width="unset"
+        height="unset"
+        justifyContent="flex-start"
+        alignItems="flex-start"
+        shrink="0"
+        alignSelf="stretch"
+        position="relative"
+        padding="24px 24px 24px 24px"
+        {...getOverrideProps(overrides, "Content")}
+      >
+        <Flex
+          gap="16px"
+          direction="row"
+          width="unset"
+          height="unset"
+          justifyContent="flex-start"
+          alignItems="center"
+          shrink="0"
+          alignSelf="stretch"
+          position="relative"
+          padding="0px 0px 0px 0px"
+          {...getOverrideProps(overrides, "Edit Profile")}
+        >
+          <Text
+            fontFamily="Inter"
+            fontSize="16px"
+            fontWeight="700"
+            color="rgba(13,26,38,1)"
+            lineHeight="20px"
+            textAlign="left"
+            display="block"
+            direction="column"
+            justifyContent="unset"
+            width="unset"
+            height="unset"
+            gap="unset"
+            alignItems="unset"
+            shrink="0"
+            position="relative"
+            padding="0px 0px 0px 0px"
+            whiteSpace="pre-wrap"
+            children="Add Car"
+            {...getOverrideProps(overrides, "Add Car")}
+          ></Text>
+        </Flex>
+        <Divider
+          width="unset"
+          height="1px"
+          shrink="0"
+          alignSelf="stretch"
+          size="small"
+          orientation="horizontal"
+          {...getOverrideProps(overrides, "Divider29766917")}
+        ></Divider>
+        <Flex
+          gap="16px"
+          direction="column"
+          width="unset"
+          height="unset"
+          justifyContent="flex-start"
+          alignItems="flex-start"
+          shrink="0"
+          alignSelf="stretch"
+          position="relative"
+          padding="0px 0px 0px 0px"
+          {...getOverrideProps(overrides, "Forms")}
+        >
+          <TextField
+            width="unset"
+            height="unset"
+            label="Name"
+            placeholder="Enter car name"
+            shrink="0"
+            alignSelf="stretch"
+            size="default"
+            isDisabled={false}
+            labelHidden={false}
+            variation="default"
+            {...getOverrideProps(overrides, "TextField29766922")}
+          ></TextField>
+          <TextField
+            width="unset"
+            height="unset"
+            label="Price per day (USD)"
+            placeholder="Enter a price"
+            shrink="0"
+            alignSelf="stretch"
+            size="default"
+            isDisabled={false}
+            labelHidden={false}
+            variation="default"
+            {...getOverrideProps(overrides, "TextField29766923")}
+          ></TextField>
+          <TextField
+            width="unset"
+            height="unset"
+            label="Description"
+            placeholder="Enter a description"
+            shrink="0"
+            alignSelf="stretch"
+            size="default"
+            isDisabled={false}
+            labelHidden={false}
+            variation="default"
+            {...getOverrideProps(overrides, "TextField29766924")}
+          ></TextField>
+        </Flex>
+        <TextField
+          width="unset"
+          height="unset"
+          label="Image Link"
+          placeholder="Enter a image url"
+          shrink="0"
+          alignSelf="stretch"
+          size="default"
+          isDisabled={false}
+          labelHidden={false}
+          variation="default"
+          {...getOverrideProps(overrides, "TextField36462711")}
+        ></TextField>
+        <Divider
+          width="unset"
+          height="1px"
+          shrink="0"
+          alignSelf="stretch"
+          size="small"
+          orientation="horizontal"
+          {...getOverrideProps(overrides, "Divider29766925")}
+        ></Divider>
+        <Button
+          width="unset"
+          height="unset"
+          shrink="0"
+          size="default"
+          isDisabled={false}
+          variation="primary"
+          children="Save"
+          {...getOverrideProps(overrides, "Button")}
+        ></Button>
+      </Flex>
+    </Flex>
+  );
+}
